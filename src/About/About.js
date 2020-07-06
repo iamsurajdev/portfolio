5946ab8ff3304bf59bde9dc345ec13963d22597c
surajbiswas367 <surajbiswas367@gmail.com>
2020-07-06 11:48:16 +0530

    Structure the layout and change the anoing react favicon

--- src/About/About.js
+++ src/About/About.js
@@ -1,14 +1,22 @@
 import React from "react";
 import classes from "./About.module.css";
+import AboutMeSvg from "../UI/AboutMeSvg";
 
 const About = () => {
   return (
     <div className={classes.main}>
       <div className={classes.textContent}>
         <h1 className={classes.heading}>About Me</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam sunt
+          est necessitatibus autem accusamus eaque veritatis culpa, fuga
+          temporibus nisi qui voluptatem, consequatur quasi nostrum reiciendis
+          perferendis nemo quia nihil consectetur modi, reprehenderit et illum
+          provident! Nostrum mollitia officiis non!
+        </p>
       </div>
       <div className={classes.illustration}>
-        <h1>illustration</h1>
+        <AboutMeSvg />
       </div>
     </div>
   );
