0c09425d25be9721d7f39a965002d1e59853437f
surajbiswas367 <surajbiswas367@gmail.com>
2020-07-16 23:21:54 +0530

    add resume download button

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -1,10 +1,18 @@
 import React from "react";
 import classes from "./Intro.module.css";
 import photo from "../Assets/images/myAvatar.png";
+import resume from "../Assets/Resume/Suraj Biswas Resume.pdf";
+import { Icon } from "@iconify/react";
+import downloadIcon from "@iconify/icons-carbon/download";
 
 const Intro = () => {
   return (
-    <div className="main">
+    <div className={classes.main}>
+      <div className={classes.topBar}>
+        <a href={resume} download>
+          Resume <Icon icon={downloadIcon} />
+        </a>
+      </div>
       <img src={photo} alt="profile" className={classes.profilePhoto} />
       <p className={classes.introText}>
         A Self-taught Programmer, Who{" "}
